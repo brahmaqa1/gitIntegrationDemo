4f464ea1951b4e17cf1c2703b4ca202c944a7ba9
brahmaqa1 <brahma.qa1@gmail.com>
2022-08-28 19:18:31 +0530

    first commit file

---
+++ Class1.java
@@ -0,0 +1,18 @@
+package Package1;
+
+public class Class1 // def of class or declaring the class 
+{
+	
+	public static void main(String args[])  // special method..  JVM alway 
+	{
+		// program execution always  starts from  main method only 
+		System.out.println("Hi Java "); // stmt   		
+             // semi colon stmt terminator 	 - comments 	
+		System.out.println("How are you");
+		System.out.println("I am fine");
+		
+	}
+	
+	
+
+}
